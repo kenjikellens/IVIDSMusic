e47ae59be5490dc7c8d748957dee5f2c05e0bc5c
Kenji Kellens <kenji.kellens@student.hogent.be>
2026-08-12 18:13:22 +0200

    chore: bump version configurations to v0.2.4

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.kenjigames.ividsmusic"
         minSdk = 24
         targetSdk = 36
-        versionCode = 28
-        versionName = "0.2.3"
+        versionCode = 29
+        versionName = "0.2.4"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
